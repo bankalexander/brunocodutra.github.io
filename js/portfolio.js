8f232df21e5555163989b7d421b96757220ca095
Bruno Dutra <brunocodutra@gmail.com>
2016-05-14 13:25:03 -0300

    improve responsiveness to screen size

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,12 @@
 $(function(){
+    $("#sections > li:first-child").on("activate.bs.scrollspy", function(){
+        $(this).closest("nav").addClass("expanded")
+    })
+
+    $("#sections > li:not(:first-child)").on("activate.bs.scrollspy", function(){
+        $(this).closest("nav").removeClass("expanded")
+    })
+
     $("a.scroll").on("click", function(event) {
         var href = $.attr(this, "href");
         $("html, body").animate({
